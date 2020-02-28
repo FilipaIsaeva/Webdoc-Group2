b54d516bd827aff072be56dbd878125015d76adf
Georgiana <57002566+GeorgianaSabau@users.noreply.github.com>
2020-02-28 12:01:04 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,84 @@
+/**
+ * file: js.js
+ * purpose: interactivity
+ * inspration from: https://www.w3schools.com/html/tryit.asp?filename=tryhtml5_video_js_prop
+ **/
+
+var myVideo = document.getElementById("myVideo");
+myVideo.width = 1200;
+myVideo.controls = true;
+
+function playPause() {
+    if (myVideo.paused)
+        myVideo.play();
+    else
+        myVideo.pause();
+}
+
+function makeBig() {
+    myVideo.width = 1000;
+}
+
+function makeSmall() {
+    myVideo.width = 200;
+}
+
+function makeNormal() {
+    myVideo.width = 420;
+}
+
+var myVideo = document.getElementById("findus");
+myVideo.width = 850;
+myVideo.controls = true;
+
+function playPause() {
+    if (myVideo.paused)
+        myVideo.play();
+    else
+        myVideo.pause();
+}
+
+function makeBig() {
+    myVideo.width = 1000;
+}
+
+function makeSmall() {
+    myVideo.width = 200;
+}
+
+function makeNormal() {
+    myVideo.width = 420;
+}
+
+
+
+
+
+
+
+
+console.log('Stardate 235678.89: JavaScript from js.js is up and running.');
+
+//paste your java script here **/
+// the image names should be real image names
+var myGallery = [
+  "pattern1.jpg",
+  "pattern2.jpg",
+  "pattern3.jpg",
+  "pattern4.jpg",
+  "pattern5.jpg",
+  "IMG_8583.jpg",
+    "carpets1.jpg",
+  "carpets2.jpg",
+	"carpets3.jpg",
+	"IMG_8579.jpg"
+  
+];
+
+//create the html **/
+for (i = 0; i < myGallery.length; i++) {
+	// note the shorthand code below
+  exhibition.innerHTML += '<img src="images/'
+  +myGallery[i]
+  +'"alt="From my gallery" class="ph">';
+}
